4d6783f713b2aa1c4916e933f9d7840ace2c276f
Tudorache Leonard Valentin <valileo2000@gmail.com>
2024-04-07 23:04:29 +0300

    feat: add maker bundle

--- config/bundles.php
+++ config/bundles.php
@@ -8,4 +8,5 @@ return [
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
     Symfony\UX\React\ReactBundle::class => ['all' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
 ];
